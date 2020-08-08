84b63716537510c25e027dcdcd50280ef68e9e09
Justin Ong <justin.ong.97@gmail.com>
2020-08-08 21:01:49 +0800

    Add test runner

---
+++ test/main/test_runner.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "unity.h"
+
+extern "C" {
+    void app_main();
+}
+
+static void print_banner(const char* text);
+
+void app_main(void)
+{
+    print_banner("Running all the registered tests");
+    UNITY_BEGIN();
+    unity_run_all_tests();
+    UNITY_END();
+}
+
+static void print_banner(const char* text)
+{
+    printf("\n#### %s #####\n\n", text);
+}
